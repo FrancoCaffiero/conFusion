fd1b7e92b79bacf4b47f19e8af94018ce3e27063
FrancoCaffiero <FrancoCaffiero@gmail.com>
2020-07-03 15:46:14 +1200

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@
 
 var gulp = require("gulp"),
   sass = require("gulp-sass"),
-  browserSync = require("browser-sync");
+  browserSync = require("browser-sync"),
+  del = require("del"),
+  imagemin = require("gulp-imagemin"),
+  uglify = require("gulp-uglify"),
+  usemin = require("gulp-usemin"),
+  rev = require("gulp-rev"),
+  cleanCss = require("gulp-clean-css"),
+  flatmap = require("gulp-flatmap"),
+  htmlmin = require("gulp-htmlmin");
 
 gulp.task("sass", function () {
   return gulp
@@ -16,12 +24,7 @@ gulp.task("sass:watch", function () {
 });
 
 gulp.task("browser-sync", function () {
-  var files = [
-    "./*.html",
-    "./css/*.scss",
-    "./js/*.js",
-    "./img/*.{png,jpg,gif}",
-  ];
+  var files = ["./*.html", "./css/*.css", "./js/*.js", "./img/*.{png,jpg,gif}"];
   browserSync.init(files, {
     server: {
       baseDir: "./",
@@ -34,4 +37,57 @@ gulp.task(
   gulp.series("browser-sync", function () {
     gulp.start("sass:watch");
   })
+);
+
+gulp.task("clean", function () {
+  return del(["dist"]);
+});
+
+gulp.task("copyfonts", function () {
+  return gulp
+    .src("node_modules/font-awesome/fonts/*.*")
+    .pipe(gulp.dest("./dist/fonts"));
+});
+
+gulp.task("imagemin", function () {
+  return gulp
+    .src("img/*.{png,jpg,gif}")
+    .pipe(
+      imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })
+    )
+    .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("usemin", function () {
+  return gulp
+    .src("./*.html")
+    .pipe(
+      flatmap(function (stream, file) {
+        return stream.pipe(
+          usemin({
+            css: [rev()],
+            html: [
+              function () {
+                return htmlmin({ collapseWhiteSpace: true });
+              },
+            ],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(), "concat"],
+          })
+        );
+      })
+    )
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task(
+  "build",
+  gulp.series(
+    "clean",
+    gulp.parallel("copyfonts", "imagemin", "usemin"),
+    function (done) {
+      done();
+    }
+  )
 );
